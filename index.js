499734400d01951ed542056d800a882ecd07a010
Phillip DiPhillipo <diphillipophillip@gmail.com>
2018-09-14 16:54:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your functions and code here
+function destructivelyAppendKitten(name){
+return kittens.push(name)
+}
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(){
+  return kittens.pop(name)
+}
+
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift()
+}
+
+function appendKitten(name){
+  return kittens.concat(name)
+}
+
+function prependKitten(name){
+  return kittens.concat(name)
+}
