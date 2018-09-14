34827b5283df2d480b3fd804113bc9d262b24130
Phillip DiPhillipo <diphillipophillip@gmail.com>
2018-09-14 17:02:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function appendKitten(name){
 }
 
 function prependKitten(name){
-  var newKittens = [...Kitten]
+  var newKittens = [...Kittens]
   newKittens.unshift(name)
   return newKittens
 }
